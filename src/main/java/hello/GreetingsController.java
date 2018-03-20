cea2d83de24fe52dc5fa9696974ad9d75fac4c45
mircs <stan.mircea.vlad@gmail.com>
2018-03-20 23:49:31 +0200

    webSocket with Stomp

---
+++ src/main/java/hello/GreetingsController.java
@@ -0,0 +1,16 @@
+package hello;
+
+import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.handler.annotation.SendTo;
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class GreetingsController {
+
+    @MessageMapping("/hello")
+    @SendTo("/topic/greetings")
+    public Greeting greeting(HelloMessage message) throws Exception {
+        Thread.sleep(1000);
+        return new Greeting(String.format("Hello, %s!", message.getName()));
+    }
+}
